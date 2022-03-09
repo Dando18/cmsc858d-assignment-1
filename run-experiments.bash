f4357feb9f8c519059f82d92bd74304a2eaba3fa
Daniel Nichols <dando18studios@gmail.com>
2022-03-08 21:49:07 -0500

    fix header formatting and larger numbers

--- run-experiments.bash
+++ run-experiments.bash
@@ -9,8 +9,7 @@ TESTS="./bin/tests"
 DATA_DIR="./data"
 RANK_HEADER="problem,bitvector_size,num_rank_calls,num_iter,overhead,avg_duration"
 SELECT_HEADER="problem,bitvector_size,num_select_calls,num_iter,overhead,avg_duration"
-SPARSEARRAY_HEADER="problem,array_size,sparsity,num_func_calls,dense_overhead,sparse_overhead,avg_append_duration,\
-                    avg_getatindex_duration,avg_getatrank_duration"
+SPARSEARRAY_HEADER="problem,array_size,sparsity,num_func_calls,dense_overhead,sparse_overhead,avg_append_duration,avg_getatindex_duration,avg_getatrank_duration"
 
 BOUNDS_CHECKING_FLAG="NO_BOUNDS_CHECKING=1"
 
@@ -37,14 +36,14 @@ mkdir -p ${DATA_DIR}
 # -- Rank --
 echo "Running Rank experiment..."
 echo ${RANK_HEADER} > ${DATA_DIR}/rank-results.csv
-for N in 100 1000 10000 100000 1000000; do
+for N in 100 1000 10000 100000 1000000 10000000; do
     ${EXEC} rank ${N} 100000 >> ${DATA_DIR}/rank-results.csv
 done
 
 # -- Select --
 echo "Running Select experiment..."
 echo ${SELECT_HEADER} > ${DATA_DIR}/select-results.csv
-for N in 100 1000 10000 100000 1000000; do
+for N in 100 1000 10000 100000 1000000 10000000; do
     ${EXEC} select ${N} 100000 >> ${DATA_DIR}/select-results.csv
 done
 
